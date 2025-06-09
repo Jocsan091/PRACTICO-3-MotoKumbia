9439faad9df052bb7a5fb9b97512feecde85c7f2
Dominus <jcc2317@gmail.com>
2025-06-08 20:42:33 -0400

    creacion de modulos

--- MotoKumbia-P3/app/Http/Controllers/MotorcycleController.php
+++ MotoKumbia-P3/app/Http/Controllers/MotorcycleController.php
@@ -43,25 +43,13 @@ class MotorcycleController extends Controller
 
     public function edit(Motorcycle $modulo2)
     {
-        $customers = Customer::all();
-        return view('modulo2.edit', compact('modulo2', 'customers'));
+
     }
 
 
     public function update(Request $request, Motorcycle $modulo2)
     {
-        $request->validate([
-            'nombreMoto' => 'required|string|max:255',
-            'patente' => 'required|string|max:6',
-            'en_taller' => 'boolean',
-            'customer_id' => 'nullable|exists:customers,id'
-        ]);
-
-        $request->merge(['en_taller' => $request->has('en_taller')]);
-
-        $modulo2->update($request->all());
-
-        return redirect()->route('modulo2.index')->with('success', 'Motocicleta actualizada correctamente.');
+        
     }
 
 
